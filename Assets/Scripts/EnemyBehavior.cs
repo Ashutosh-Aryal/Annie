a3897f942a9acbd22cbc40e84001d4accf2d9ed4
Ashutosh-Aryal <aryala@usc.edu>
2020-09-22 20:55:20 -0700

    Added character movement + annie sound placement animation + attack

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EnemyBehavior : MonoBehaviour
+{ 
+    private const int RIGHT_CLICK = 1;
+    private const int MAX_ENEMY_COUNT = 100;
+
+    [SerializeField] private Transform[] m_Waypoints;
+
+    private int m_WaypointIndex = 0;
+    private int m_RandValue;
+
+    private Rigidbody2D myRigidbody2D;
+
+    private static HashSet<int> s_AssignedEnemyNumbers = new HashSet<int>();
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(RIGHT_CLICK))
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            AnnieBehavior.SetRightClickStartPosition(gameObject.name);
+        }        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = 0;
+
+        do {
+            rand = Random.Range(0, MAX_ENEMY_COUNT);
+        } while (s_AssignedEnemyNumbers.Contains(rand));
+
+        s_AssignedEnemyNumbers.Add(rand);
+        m_RandValue = rand;
+        gameObject.name += rand;
+
+        myRigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnDestroy()
+    {
+        s_AssignedEnemyNumbers.Remove(m_RandValue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // TODO: Check if there is a sound location to go to
+        // If not, check what the closest waypoint is from current location and go towards it
+        // and update the current index to reflect being at that waypoint
+    }
+}
--- Assets/Scripts/MundoMovement.cs
+++ Assets/Scripts/MundoMovement.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Net.Mail;
+using TMPro;
+using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class MundoMovement : MonoBehaviour {    
-    enum AnimationType
+    private enum AnimationType
     {
         Idle,
         MovingRight, 
@@ -14,10 +17,24 @@ public class MundoMovement : MonoBehaviour {
         StartAttacking,
         StopAttacking
     }
-
-    private enum DirectionFacing
+    private enum MovementDirection
     {
-        North, East, South, West
+        Idle,
+        North,
+        Northeast,
+        East,
+        Southeast,
+        South,
+        Southwest,
+        West,
+        Northwest
+    }
+
+    private enum MundoState
+    {
+        CanPutDownAnnie,
+        CanPickUpAnnie, 
+        CannotInteractWithAnnie
     }
 
     private const KeyCode UP_KEY = KeyCode.W;
@@ -27,13 +44,13 @@ public class MundoMovement : MonoBehaviour {
     private const KeyCode INTERACT_WITH_ANNIE_KEY = KeyCode.F;
     private const KeyCode ATTACK_KEY = KeyCode.Space;
 
-    private const float MAX_INTERACT_DISTANCE = 1.0f;
+    private const float MOVEMENT_SPEED = 30.0f;
 
     private static AnimationType se_AnimationType = AnimationType.Idle;
-    private static DirectionFacing se_DirectionFacing = DirectionFacing.East;
-    private static bool sb_IsHoldingAnnie = true;
-    private static bool sb_CanInteract = true;
-
+    private static MovementDirection se_MovementDirection = MovementDirection.Idle;
+    private static MovementDirection se_LastRealMovementDirection = MovementDirection.East;
+    private static MundoState se_MundoState = MundoState.CannotInteractWithAnnie;
+    
     private static List<GameObject> sL_AvailableEnemiesToAttack = new List<GameObject>();
 
     private static Rigidbody2D myRigidbody;
@@ -61,59 +78,54 @@ public class MundoMovement : MonoBehaviour {
             sL_AvailableEnemiesToAttack.Add(collision.gameObject);
         } else if(isCollisionAnnie)
         {
-
+            se_MundoState = MundoState.CanPickUpAnnie;
         }
 
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        bool isCollisionEnemy = collision.gameObject.CompareTag(m_EnemyPrefab.tag);
+        bool isCollisionAnnie = collision.gameObject.CompareTag(m_AnnieObject.tag);
+
+        if (isCollisionEnemy)
+        {
+            sL_AvailableEnemiesToAttack.Remove(collision.gameObject);
+        }
+        else if (isCollisionAnnie)
+        {
+            se_MundoState = MundoState.CannotInteractWithAnnie;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheckInteract();
+        CheckInput();
+
         UpdateMovement();
         UpdateAnimation();
     }
 
-    private void CheckInteract()
+    private void CheckInput()
     {
-        if(sb_IsHoldingAnnie) {
-            sb_CanInteract = true; return;
-        }
-
-        Vector3 mundoPosition = gameObject.transform.position;
-        Vector3 anniePosition = m_AnnieObject.transform.position;
-        Vector3 distanceVector = mundoPosition - anniePosition;
-
-        sb_CanInteract = distanceVector.magnitude <= MAX_INTERACT_DISTANCE; // TODO: Change this so its based on BoxCollider2D instead
-    }
-
-    private void UpdateMovement()
-    {
-        bool didPressUp = Input.GetKey(UP_KEY);
-        bool didPressDown = Input.GetKey(DOWN_KEY);
-        bool didPressLeft = Input.GetKey(LEFT_KEY);
-        bool didPressRight = Input.GetKey(RIGHT_KEY);
-        bool didPressInteract = Input.GetKey(INTERACT_WITH_ANNIE_KEY);
         bool didPressAttack = Input.GetKeyDown(ATTACK_KEY);
 
-        bool shouldAttack = !sb_IsHoldingAnnie && didPressAttack;
+        bool shouldAttack = se_MundoState != MundoState.CanPutDownAnnie && didPressAttack;
+        
         if(shouldAttack)
         {
             OnAttack();
+            se_MovementDirection = MovementDirection.Idle;
             return;
         }
 
+        bool didPressInteract = Input.GetKeyDown(INTERACT_WITH_ANNIE_KEY);
+        bool shouldInteract = (se_MundoState != MundoState.CannotInteractWithAnnie) && didPressInteract;
 
-        bool shouldInteract = sb_CanInteract && didPressInteract;
-
-        if(shouldInteract && !shouldAttack)
+        if(shouldInteract)
         {
-            if(sb_IsHoldingAnnie)
+            if(se_MundoState == MundoState.CanPutDownAnnie)
             {
                 PutDownAnnie();
             } else
@@ -121,27 +133,231 @@ public class MundoMovement : MonoBehaviour {
                 PickUpAnnie();
             }
         }
+
+        AnimationType animationType;
+        bool shouldSwapAnimationType;
+
+        bool didPressUp = Input.GetKey(UP_KEY);
+        bool didPressDown = Input.GetKey(DOWN_KEY);
+        bool didPressLeft = Input.GetKey(LEFT_KEY);
+        bool didPressRight = Input.GetKey(RIGHT_KEY);
+
+        bool isCurrentAnimationUp = se_AnimationType == AnimationType.MovingUp;
+        bool isCurrentAnimationDown = se_AnimationType == AnimationType.MovingDown;
+        bool isCurrentAnimationRight = se_AnimationType == AnimationType.MovingRight;
+        bool isCurrentAnimationLeft = se_AnimationType == AnimationType.MovingLeft;
+
+        if(didPressUp && didPressRight)
+        {
+            se_MovementDirection = MovementDirection.Northeast;
+            animationType = AnimationType.MovingUp;
+            shouldSwapAnimationType = !isCurrentAnimationUp && !isCurrentAnimationRight;
+
+        } else if(didPressUp && didPressLeft)
+        {
+            se_MovementDirection = MovementDirection.Northwest;
+            animationType = AnimationType.MovingUp;
+            shouldSwapAnimationType = !isCurrentAnimationUp && !isCurrentAnimationLeft;
+        } else if(didPressDown && didPressRight)
+        {
+            se_MovementDirection = MovementDirection.Southeast;
+            animationType = AnimationType.MovingDown;
+            shouldSwapAnimationType = !isCurrentAnimationDown && !isCurrentAnimationRight;
+        } else if(didPressDown && didPressLeft)
+        {
+            se_MovementDirection = MovementDirection.Southwest;
+            animationType = AnimationType.MovingDown;
+            shouldSwapAnimationType = !isCurrentAnimationDown && !isCurrentAnimationLeft;
+        } else if(didPressUp)
+        {
+            se_MovementDirection = MovementDirection.North;
+            animationType = AnimationType.MovingUp;
+            shouldSwapAnimationType = !isCurrentAnimationUp;
+        } else if(didPressLeft)
+        {
+            se_MovementDirection = MovementDirection.West;
+            animationType = AnimationType.MovingLeft;
+            shouldSwapAnimationType = !isCurrentAnimationLeft;
+        } else if(didPressRight)
+        {
+            se_MovementDirection = MovementDirection.East;
+            animationType = AnimationType.MovingRight;
+            shouldSwapAnimationType = !isCurrentAnimationRight;
+        } else if(didPressDown)
+        {
+            se_MovementDirection = MovementDirection.South;
+            animationType = AnimationType.MovingDown;
+            shouldSwapAnimationType = !isCurrentAnimationDown;
+        } else
+        {
+            se_MovementDirection = MovementDirection.Idle;
+            animationType = AnimationType.Idle;
+            shouldSwapAnimationType = se_AnimationType != AnimationType.Idle;
+        }
+
+        if(se_MovementDirection != MovementDirection.Idle)
+        {
+            se_LastRealMovementDirection = se_MovementDirection;
+        }
+
+        if(shouldSwapAnimationType)
+        {
+            se_AnimationType = animationType;
+        }
     }
 
     private void OnAttack()
     {
-
-
         se_AnimationType = AnimationType.StartAttacking;
+
+        MovementDirection md = MovementDirection.Idle;
+        float closestDistance = 1000.0f;
+        GameObject bestKillOption = new GameObject();
+
+        foreach(GameObject enemy in sL_AvailableEnemiesToAttack) {
+            Vector3 enemyLocation = enemy.transform.position;
+            Vector3 myLocation = gameObject.transform.position;
+
+            Vector3 distanceVector = enemyLocation - myLocation;
+            Vector2 distanceVector2D = new Vector2(distanceVector.x, distanceVector.y);
+            float distance = distanceVector2D.magnitude;
+
+            distanceVector2D.Normalize();
+
+            MovementDirection enemyDirectionFromPlayer = GetDirection(distanceVector2D);
+
+            bool isLastFoundMovementDirectionInDirectionOfPlayer = md == se_LastRealMovementDirection;
+            bool isCurrentEnemyInDirectionOfPlayer = enemyDirectionFromPlayer == se_LastRealMovementDirection;
+            bool isCurrentDistanceLessThanPriorFoundDistance = distance < closestDistance;
+
+            if(md == MovementDirection.Idle || (!isLastFoundMovementDirectionInDirectionOfPlayer && isCurrentEnemyInDirectionOfPlayer)) {
+                md = enemyDirectionFromPlayer;
+                closestDistance = distance;
+                bestKillOption = enemy;
+            } else if(isLastFoundMovementDirectionInDirectionOfPlayer && isCurrentEnemyInDirectionOfPlayer && isCurrentDistanceLessThanPriorFoundDistance) {
+                closestDistance = distance;
+                bestKillOption = enemy;
+            } else if(IsLeftArgCloser(enemyDirectionFromPlayer, md) && isCurrentDistanceLessThanPriorFoundDistance)
+            {
+                md = enemyDirectionFromPlayer;
+                closestDistance = distance;
+                bestKillOption = enemy;
+            }
+        }
+
+        Destroy(GameObject.Find(bestKillOption.name));
+    }
+
+    private bool IsLeftArgCloser(MovementDirection newDirection, MovementDirection oldDirection)
+    {
+        return Mathf.Abs(((int)oldDirection - (int)se_LastRealMovementDirection)) > Mathf.Abs((int)newDirection - (int)se_LastRealMovementDirection); 
+    }
+
+    private const float DIFFERENCE_RANGE = 0.1f;
+
+    private MovementDirection GetDirection(Vector2 directionVector)
+    {
+        MovementDirection[] directionChoices = new MovementDirection[3];
+        float difference;
+
+        if(directionVector.x > 0.0f && directionVector.y > 0.0f)
+        {
+            directionChoices[0] = MovementDirection.East;
+            directionChoices[1] = MovementDirection.Northeast;
+            directionChoices[2] = MovementDirection.North;
+            difference = directionVector.x - directionVector.y;
+            
+        } else if(directionVector.x > 0.0f && directionVector.y < 0.0f)
+        {
+            directionChoices[0] = MovementDirection.East;
+            directionChoices[1] = MovementDirection.Southeast;
+            directionChoices[2] = MovementDirection.South;
+            difference = directionVector.x - Mathf.Abs(directionVector.y);
+        } else if(directionVector.x < 0.0f && directionVector.y > 0.0f)
+        {
+            directionChoices[0] = MovementDirection.West;
+            directionChoices[1] = MovementDirection.Northwest;
+            directionChoices[2] = MovementDirection.North;
+            difference = Mathf.Abs(directionVector.x) - directionVector.y;
+        } else
+        {
+            directionChoices[0] = MovementDirection.West;
+            directionChoices[1] = MovementDirection.Southwest;
+            directionChoices[2] = MovementDirection.South;
+            difference = Mathf.Abs(directionVector.x) - Mathf.Abs(directionVector.y);
+        }
+
+        if(difference >= DIFFERENCE_RANGE)
+        {
+            return directionChoices[0];
+        } else if(difference <= -DIFFERENCE_RANGE)
+        {
+            return directionChoices[2];
+        } else
+        {
+            return directionChoices[1];
+        }
     }
 
     private void PutDownAnnie()
     {
-        sb_IsHoldingAnnie = false;
+        se_MundoState = MundoState.CanPickUpAnnie;
         m_AnnieObject.transform.position = gameObject.transform.position;
-        m_AnnieObject.GetComponent<SpriteRenderer>().enabled = true;
+        //m_AnnieObject.GetComponent<SpriteRenderer>().enabled = true;
+        m_AnnieObject.SetActive(true);
     } 
 
     private void PickUpAnnie()
     {
-        sb_IsHoldingAnnie = true;
-        m_AnnieObject.GetComponent<SpriteRenderer>().enabled = false;
+        se_MundoState = MundoState.CanPutDownAnnie;
+        m_AnnieObject.SetActive(false);
+        //m_AnnieObject.GetComponent<SpriteRenderer>().enabled = false;
     }
+
+    private void UpdateMovement()
+    {
+        switch(se_MovementDirection)
+        {
+            case MovementDirection.North:
+                myRigidbody.velocity = Vector2.up * MOVEMENT_SPEED;
+                break;
+
+            case MovementDirection.East:
+                myRigidbody.velocity = Vector2.right * MOVEMENT_SPEED;
+                break;
+
+            case MovementDirection.South:
+                myRigidbody.velocity = Vector2.down * MOVEMENT_SPEED;
+                break;
+
+            case MovementDirection.West:
+                myRigidbody.velocity = Vector2.left * MOVEMENT_SPEED;
+                break;
+
+            case MovementDirection.Northeast:
+                myRigidbody.velocity = Vector2.one.normalized * MOVEMENT_SPEED;
+                break;
+
+            case MovementDirection.Northwest:
+                myRigidbody.velocity = new Vector2(-Vector2.one.x, Vector2.one.y).normalized;
+                myRigidbody.velocity *= MOVEMENT_SPEED;
+                break;
+
+            case MovementDirection.Southeast:
+                myRigidbody.velocity = new Vector2(Vector2.one.x, -Vector2.one.y).normalized;
+                myRigidbody.velocity *= MOVEMENT_SPEED;
+                break;
+
+            case MovementDirection.Southwest:
+                myRigidbody.velocity = new Vector2(-Vector2.one.x, -Vector2.one.y).normalized;
+                myRigidbody.velocity *= MOVEMENT_SPEED;
+                break;
+
+            case MovementDirection.Idle:
+                myRigidbody.velocity = Vector2.zero;
+                break;
+        }
+    } 
 
     private void UpdateAnimation()
     {
@@ -151,7 +367,7 @@ public class MundoMovement : MonoBehaviour {
          * 4 Attacking Animations
          */
 
-        myAnimator.SetBool("isCarryingAnnie", sb_IsHoldingAnnie);
+        myAnimator.SetBool("isCarryingAnnie", se_MundoState == MundoState.CanPutDownAnnie);
 
         switch (se_AnimationType)
         {
@@ -175,25 +391,21 @@ public class MundoMovement : MonoBehaviour {
             case AnimationType.MovingLeft:
                 myAnimator.SetFloat("vertical", 0.0f);
                 myAnimator.SetFloat("horizontal", -1.0f);
-                se_DirectionFacing = DirectionFacing.West;
                 break;
 
             case AnimationType.MovingRight:
                 myAnimator.SetFloat("vertical", 0.0f);
                 myAnimator.SetFloat("horizontal", 1.0f);
-                se_DirectionFacing = DirectionFacing.East;
                 break;
 
             case AnimationType.MovingUp:
                 myAnimator.SetFloat("vertical", 1.0f);
                 myAnimator.SetFloat("horizontal", 0.0f);
-                se_DirectionFacing = DirectionFacing.North;
                 break;
 
             case AnimationType.MovingDown:
                 myAnimator.SetFloat("vertical", -1.0f);
                 myAnimator.SetFloat("horizontal", 0.0f);
-                se_DirectionFacing = DirectionFacing.North;
                 break;
         }
     }
